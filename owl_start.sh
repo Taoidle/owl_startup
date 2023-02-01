4af6e99c5e67bd32926996f441aee0c84c3e7a6b
taoidle <me@mail.taoidle.com>
2023-02-02 01:16:18 +0800

    ..

--- owl_start.sh
+++ owl_start.sh
@@ -2,10 +2,12 @@
 
 mv OwlAccessTerminal /usr/bin
 chmod 755 /usr/bin/OwlAccessTerminal
+mv OwlAprilTagProcessor /usr/bin
+chmod 755 /usr/bin/OwlAprilTagProcessor
 mkdir -p /etc/owl
-mv owl_configure.py /etc/owl
-chmod 755 /etc/owl/owl_configure.py
-sed -i "13a python3 /etc/owl/owl_configure.py" /etc/rc.local
-sed -i "14a nohup OwlAccessTerminal -c /etc/owl/config.json > /etc/owl/owl.log &" /etc/rc.local
-# reboot
-
+mv *.py /etc/owl
+chmod 755 /etc/owl/owl_terminal_configure.py /etc/owl/owl_apriltag_configure.py
+python3 /etc/owl/owl_apriltag_configure.py
+mv *.service /etc/systemd/system/
+chmod 777 owl-terminal.service owl-apriltag.service
+systemctl enable owl-terminal.service owl-apriltag.service
