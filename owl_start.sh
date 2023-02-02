5e4b08727fb37d3681decb1433766163e2215382
taoidle <me@mail.taoidle.com>
2023-02-02 14:37:21 +0800

    ..

--- owl_start.sh
+++ owl_start.sh
@@ -9,5 +9,5 @@ mv *.py /etc/owl
 chmod 755 /etc/owl/owl_terminal_configure.py /etc/owl/owl_apriltag_configure.py
 python3 /etc/owl/owl_apriltag_configure.py
 mv *.service /etc/systemd/system/
-chmod 777 owl-terminal.service owl-apriltag.service
+chmod 777 /etc/systemd/system/owl-terminal.service /etc/systemd/system/owl-apriltag.service
 systemctl enable owl-terminal.service owl-apriltag.service
